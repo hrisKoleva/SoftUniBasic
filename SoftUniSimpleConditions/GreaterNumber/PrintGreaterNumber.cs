68cd310d125079b4cddf3867e63738ba121b4be4
hristina.koleva <hristinakoleva@gmail.com>
2017-01-26 10:25:25 +0200

    Third exercises started - Simple Conditions

---
+++ SoftUniSimpleConditions/GreaterNumber/PrintGreaterNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreaterNumber
+{
+    class PrintGreaterNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two integers:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a>=b)
+            {
+                Console.WriteLine("Greater number: {0}", a);
+            }
+            else
+            {
+                Console.WriteLine("Greater number: {0}", b);
+            }
+        }
+    }
+}
---
+++ SoftUniSimpleConditions/NumbersInText/PrintTheWordForNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumbersInText
+{
+    class PrintTheWordForNumber
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> numbers = new Dictionary<int, string> { };
+
+            numbers.Add(0, "zero");
+            numbers.Add(1, "one");
+            numbers.Add(2, "two");
+            numbers.Add(3, "three");
+            numbers.Add(4, "four");
+            numbers.Add(5, "five");
+            numbers.Add(6, "six");
+            numbers.Add(7, "seven");
+            numbers.Add(8, "eight");
+            numbers.Add(9, "nine");
+
+            int input = int.Parse(Console.ReadLine());
+            if (numbers.ContainsKey(input))
+            {
+                Console.WriteLine(numbers[input]);
+            }
+            else
+            {
+                Console.WriteLine("number too big");
+            }
+        }
+    }
+}
