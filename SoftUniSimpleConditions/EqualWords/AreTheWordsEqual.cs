271069248e2a7a4bb06f184a0739711dab9ec090
hrisKoleva <hristinakoleva@gmail.com>
2017-01-28 12:18:01 +0200

    NumberBetween100and200 and EqualWords submitted

---
+++ SoftUniSimpleConditions/EqualWords/AreTheWordsEqual.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EqualWords
+{
+    class AreTheWordsEqual
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+
+            if(firstWord.ToLower().Trim().Equals(secondWord.ToLower().Trim()))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
