0ea4f41b46d1fb2e6ece7290dae79f2c6086de23
hrisKoleva <hristinakoleva@gmail.com>
2017-01-28 12:41:59 +0200

    AreaOfFigures submmitted; TimeAfter15 only project created

---
+++ SoftUniSimpleConditions/AreaOfFigures/CalculateTheArea.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfFigures
+{
+    class CalculateTheArea
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double firstSide;
+            double secondSide;
+            double area;
+
+            if(figure.Equals("square"))
+            {
+                firstSide = double.Parse(Console.ReadLine());
+                area = Math.Pow(firstSide, 2);
+                Console.WriteLine(Math.Round(area, 3));
+            }
+            else
+            {
+                return;
+            }
+
+            if (figure.Equals("rectangle"))
+            {
+                firstSide = double.Parse(Console.ReadLine());
+                secondSide = double.Parse(Console.ReadLine());
+                area = firstSide * secondSide;
+                Console.WriteLine(Math.Round(area, 3));
+            }
+
+            if (figure.Equals("circle"))
+            {
+                firstSide = double.Parse(Console.ReadLine());
+                area = Math.PI * Math.Pow(firstSide, 2);
+                Console.WriteLine(Math.Round(area, 3));
+            }
+
+            if (figure.Equals("triangle"))
+            {
+                firstSide = double.Parse(Console.ReadLine());
+                secondSide = double.Parse(Console.ReadLine());
+                area = firstSide * secondSide / 2;
+                Console.WriteLine(Math.Round(area, 3));
+            }
+        }
+    }
+}
