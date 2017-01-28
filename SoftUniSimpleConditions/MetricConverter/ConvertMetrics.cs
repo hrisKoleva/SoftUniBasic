b276ee10e865939a64fa3f02c32d6c8604812846
hrisKoleva <hristinakoleva@gmail.com>
2017-01-28 12:08:17 +0200

    MetricConverter and PasswordGuess done

---
+++ SoftUniSimpleConditions/MetricConverter/ConvertMetrics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricConverter
+{
+    class ConvertMetrics
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, double> metrics = new Dictionary<string, double>();
+
+            metrics.Add("mm", 1000);
+            metrics.Add("cm", 100);
+            metrics.Add("mi", 0.000621371192);
+            metrics.Add("in", 39.3700787);
+            metrics.Add("km", 0.001);
+            metrics.Add("ft", 3.2808399);
+            metrics.Add("yd", 1.0936133);
+
+            double value = double.Parse(Console.ReadLine());
+            string fromMetric = Console.ReadLine();
+            string toMetric = Console.ReadLine();
+            double result;
+
+            result = value * metrics[toMetric]/metrics[fromMetric];
+
+            Console.WriteLine(result + " " + toMetric);
+        }
+    }
+}
---
+++ SoftUniSimpleConditions/PasswordGuess/GuessThePassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PasswordGuess
+{
+    class GuessThePassword
+    {
+        static void Main(string[] args)
+        {
+            string password = "s3cr3t!P@ssw0rd";
+            string login = Console.ReadLine();
+
+            if(password.Equals(login))
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
