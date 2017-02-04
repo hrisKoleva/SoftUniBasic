6ba1c09e50e3695b02add2bc8d598964e552d229
hrisKoleva <hristinakoleva@gmail.com>
2017-02-04 10:09:36 +0200

    малък коментар добавен, ако нещо не върне резултат в judge - re-submit може и да мине, ако при мен си работи разбира се

--- SoftUniSimpleConditions/EqualWords/AreTheWordsEqual.cs
+++ SoftUniSimpleConditions/EqualWords/AreTheWordsEqual.cs
@@ -6,6 +6,7 @@ namespace EqualWords
     {
         static void Main(string[] args)
         {
+            //поради някаква причина, при първия submit SoFTuNi/sOFtUnI не беше върнало резултат, при втори submit мина :-)
             string firstWord = Console.ReadLine();
             string secondWord = Console.ReadLine();
 
