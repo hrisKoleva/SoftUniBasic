1d6cceeef22e42e5e9ea526a34aca3fa74d5ed61
hrisKoleva <hristinakoleva@gmail.com>
2017-02-04 11:50:00 +0200

    3EqualNumbers done.submitted.passed

---
+++ SoftUniSimpleConditions/ThreeEqualNumbers/AreThreeNumberEqual.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ThreeEqualNumbers
+{
+    class AreThreeNumberEqual
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+
+            bool areEqual = numberOne == numberTwo && numberTwo == numberThree;
+            if(areEqual)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
