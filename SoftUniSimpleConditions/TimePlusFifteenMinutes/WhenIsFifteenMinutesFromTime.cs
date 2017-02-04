fff44522205789e4b1297be51e35983ded74ab3e
hrisKoleva <hristinakoleva@gmail.com>
2017-02-04 11:36:15 +0200

    TimePlusFifteenMinutes done.submitted.passed

---
+++ SoftUniSimpleConditions/TimePlusFifteenMinutes/WhenIsFifteenMinutesFromTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TimePlusFifteenMinutes
+{
+    class WhenIsFifteenMinutesFromTime
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = new DateTime();
+
+            string inputHours = string.Format("{0:H}", Console.ReadLine());
+            string inputMinutes = string.Format("{0:mm}", Console.ReadLine());
+
+            time = time.AddHours(double.Parse(inputHours));
+            time = time.AddMinutes(double.Parse(inputMinutes));
+
+            DateTime endTime = time.AddMinutes(15);
+
+            Console.WriteLine("{0:H:mm}", endTime);
+        }
+    }
+}
