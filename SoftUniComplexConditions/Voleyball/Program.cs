fec32734b519a88d761f22c0d4680b168beb802c
hristina.koleva <hristinakoleva@gmail.com>
2017-02-13 15:00:12 +0200

    SoftUni Fourth Homework - 12th Voleyball task done submited passed

---
+++ SoftUniComplexConditions/Voleyball/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yearType = Console.ReadLine();
+            int holidays = int.Parse(Console.ReadLine());
+            int daysHome = int.Parse(Console.ReadLine());
+
+            int weekends = 48;
+            double result;
+
+            weekends = weekends - daysHome;
+            var numberOfSaturdays = weekends * 3.0 / 4;
+            var numberOfHolidays = holidays * 2.0 / 3;
+            result = numberOfHolidays + numberOfSaturdays + daysHome;
+
+            if (yearType.Equals("leap"))
+            {
+               
+                result += 0.15 * result;
+                Console.WriteLine(Math.Truncate(result));
+                
+            }
+            else if(yearType.Equals("normal"))
+            {
+                Console.WriteLine(Math.Truncate(result));
+
+            }
+        }
+    }
+}
--- SoftUniComplexConditions/Voleyball/Properties/AssemblyInfo.cs
+++ SoftUniComplexConditions/Voleyball/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GardenerHarvestProfit")]
+[assembly: AssemblyTitle("Voleyball")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GardenerHarvestProfit")]
+[assembly: AssemblyProduct("Voleyball")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f6caddcc-b0b6-4873-a4c3-f3b68dca80ec")]
+[assembly: Guid("bbf94605-0f7b-4cc8-8dcd-42fdb90d0a09")]
 
 // Version information for an assembly consists of the following four values:
 //
