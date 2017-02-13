82284467d4b00ad6dfbd85c4398aa59cf40c3cac
hristina.koleva <hristinakoleva@gmail.com>
2017-02-13 19:39:00 +0200

    4thHomework 3rdTask IsPointInRectangle - done.Submitted.Passed

---
+++ SoftUniComplexConditions/ThirdPointInRectangle/PointInRectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ThirdPointInRectangle
+{
+    class PointInRectangle
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            if(x1 < x2 && y1 < y2)
+            {
+                if (x <= x2 && x >= x1 && y <= y2 && y >= y1)
+                {
+                    Console.WriteLine("Inside");
+                }
+                else
+                    Console.WriteLine("Outside");
+            }
+            else
+            {
+                Console.WriteLine("Invalid rectangle");
+            }
+        }
+    }
+}
