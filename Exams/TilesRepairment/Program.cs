4d3188efb3e0068ba71bb2ae465ec946ef417ebe
hristina.koleva <hristinakoleva@gmail.com>
2017-02-16 10:38:44 +0200

    Exam April - Tiles - Submitted Passed

---
+++ Exams/TilesRepairment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TilesRepairment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double square = double.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
+            double tileLength = double.Parse(Console.ReadLine());
+
+            double benchWidth = double.Parse(Console.ReadLine());
+            double benchLength = double.Parse(Console.ReadLine());
+
+            double tiles = (square * square - benchLength * benchWidth) / (tileLength * tileWidth);
+
+            double time = tiles * 0.2;
+
+            Console.WriteLine(tiles);
+            Console.WriteLine(time);
+        }
+    }
+}
