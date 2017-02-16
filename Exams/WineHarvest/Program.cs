d07ba81afc3b7f8773f540a8046327cd96704f0c
hristina.koleva <hristinakoleva@gmail.com>
2017-02-16 11:51:48 +0200

    Exam - 3/22* - 17 July - Wine Harvest

---
+++ Exams/WineHarvest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WineHarvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareMetersArea = int.Parse(Console.ReadLine());
+            double kilogramsPerSquareMeter = double.Parse(Console.ReadLine());
+            int wineGoalInLitres = int.Parse(Console.ReadLine());
+            int numberOfWorkers = int.Parse(Console.ReadLine());
+
+            double kilograms = 0.4 * squareMetersArea * kilogramsPerSquareMeter;
+            double litres = kilograms/2.5;
+
+            if( litres < wineGoalInLitres)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(wineGoalInLitres-litres)} liters wine needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(litres)} liters.");
+                Console.WriteLine($"{Math.Ceiling(litres-wineGoalInLitres)} liters left -> {Math.Ceiling((litres - wineGoalInLitres)/numberOfWorkers)} liters per person.");
+            }
+
+        }
+    }
+}
