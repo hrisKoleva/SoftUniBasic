6d3660577d1ec3dc60ef9c5b9bfa60345698af24
hristina.koleva <hristinakoleva@gmail.com>
2017-02-16 12:22:36 +0200

    Exam - 03/23* - 28 August - Submitted. Passed

---
+++ Exams/CompanyHoursEnough/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CompanyHoursEnough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int estimatedHours = int.Parse(Console.ReadLine());
+            int availableDays = int.Parse(Console.ReadLine());
+            int resources = int.Parse(Console.ReadLine());
+
+            double normal = availableDays * 8;
+            double overtime = availableDays * resources * 2;
+            double education = normal * 10/100;
+            double availableTotalHours = normal + overtime - education;
+
+          
+            if (estimatedHours <= availableTotalHours)
+            {
+                Console.WriteLine($"Yes!{Math.Floor(availableTotalHours-estimatedHours)} hours left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough time!{Math.Ceiling(estimatedHours - availableTotalHours)} hours needed.");
+
+            }
+        }
+    }
+}
