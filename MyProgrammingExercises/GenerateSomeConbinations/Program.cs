fa951421e1a8e4fa83a2c2452bbd527e50e0051e
hristina.koleva <hristinakoleva@gmail.com>
2017-02-22 15:09:24 +0200

    my programming exercise but most important 5th Homework - Loops - Last Task Done Submitted Passed - 12th Task - Equal Pairs

---
+++ MyProgrammingExercises/GenerateSomeConbinations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateSomeConbinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] words = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{words[j]}, ");
+
+                    for (int m = 0; m < n; m++)
+                    {
+                        Console.WriteLine($"{words[j]}, {words[m]}");
+
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ SonftUniLoops/TwelfthEqualPairs/EqualPairs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TwelfthEqualPairs
+{
+    class EqualPairs
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[2 * n];
+            int[] sumOfPairs = new int[n];
+            int tempSum = 0;
+            int difference = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    numbers[j] = int.Parse(Console.ReadLine());
+                    sumOfPairs[i] += numbers[j];
+                }
+            }
+
+            tempSum = sumOfPairs[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (sumOfPairs[i] >= tempSum)
+                {
+                    difference = sumOfPairs[i] - sumOfPairs[i - 1];
+                    tempSum = sumOfPairs[i];
+                }
+                else if (sumOfPairs[i] < tempSum)
+                {
+                    tempSum = sumOfPairs[i - 1];
+                    difference = tempSum - sumOfPairs[i];
+                }
+
+            }
+
+            if (difference != 0)
+            {
+                Console.WriteLine($"No, maxdiff={Math.Abs(difference)}");
+            }
+            else
+            {
+                Console.WriteLine($"Yes, value={tempSum}");
+            }
+
+        }
+    }
+}
