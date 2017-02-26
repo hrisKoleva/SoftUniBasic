2e32d81f4e6db97d1aaa7196e5c536174cad2bc8
hrisKoleva <hristinakoleva@gmail.com>
2017-02-26 12:08:19 +0200

    6th Homework Rhombus & Diamond Done submitted passed

---
+++ SoftUniDrawingWithLoops/F_RhombusOfStars/RhombusOfStars.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace F_RhombusOfStars
+{
+    class RhombusOfStars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(new string(' ', (n - i)));
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j != i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+
+                    }
+                }
+                Console.WriteLine(new string(' ', (n - i)));
+            }
+            for (int i = n-1; i >= 1; i--)
+            {
+                Console.Write(new string(' ', (n - i)));
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j != i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+
+                    }
+                }
+                Console.WriteLine(new string(' ', (n - i)));
+            }
+        }
+    }
+}
---
+++ SoftUniDrawingWithLoops/G_Diamond/Diamond.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace J_Diamond
+{
+    class Diamond
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //нечетно n
+            if (n % 2 == 1)
+            {
+                for (int i = 1; i <= (n + 1) / 2; i++)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+                        if ((j == ((n + 1) / 2) - (i - 1)) || j == ((n + 1) / 2) + (i - 1))
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write("-");
+
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = (n + 1) / 2 - 1; i >= 1; i--)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+
+                        if ((j == ((n + 1) / 2) - (i - 1)) || j == ((n + 1) / 2) + (i - 1))
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write("-");
+
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else //четно
+            {
+                for (int i = 1; i <= n / 2; i++)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+                        if (j == (n/2 - (i - 1)) || j == ((n/2+1) + (i - 1)))
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write("-");
+
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = (n/2)-1; i >= 1; i--)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+
+                        if (j == (n/2 - (i - 1)) || j == ((n/2+1) + (i - 1)))
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write("-");
+
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
