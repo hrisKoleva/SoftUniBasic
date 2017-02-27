6049b464d85627588e638717b7d301a47b9caec4
hristina.koleva <hristinakoleva@gmail.com>
2017-02-27 18:56:08 +0200

    Exams 6th March Draw a Fort Submitted Passed - ugly solution

---
+++ SoftUniDrawingWithLoops/K_Fortress/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_Fortress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ширина 2 * n колони и  височина n реда
+            /* 8
+             *  012345
+                /^^^^\____ /^^^^\
+                |               |
+                |               |
+                |               |
+                |               |
+                |               |
+                |     ____      |
+                \____/    \____/
+            
+             */
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if (n >= 5)
+                {
+                    if (n % 2 == 1)
+                    {
+                        if (i == 0)
+                        {
+                            Console.Write('/');
+                            Console.Write(new string('^', n / 2));
+                            Console.Write('\\');
+                            Console.Write(new string('_', n - 3));
+                            Console.Write('/');
+                            Console.Write(new string('^', n / 2));
+                            Console.WriteLine('\\');
+                        }
+                        else if (i == n - 1)
+                        {
+                            Console.Write('\\');
+                            Console.Write(new string('_', n / 2));
+                            Console.Write('/');
+                            Console.Write(new string(' ', n - 3));
+                            Console.Write('\\');
+                            Console.Write(new string('_', n / 2));
+                            Console.WriteLine('/');
+                        }
+                        else if (i == n - 2)
+                        {
+                            Console.Write("| ");
+                            Console.Write(new string(' ', n / 2));
+                            Console.Write(new string('_', n - 3));
+                            Console.Write(new string(' ', n / 2));
+                            Console.WriteLine(" |");
+                        }
+                        else
+                        {
+                            Console.Write("|");
+                            Console.Write(new string(' ', 2 * n - 2));
+                            Console.WriteLine("|");
+                        }
+                    }
+                    else
+                    {
+                        if (i == 0)
+                        {
+                            Console.Write('/');
+                            Console.Write(new string('^', n / 2));
+                            Console.Write('\\');
+                            Console.Write(new string('_', n - 4));
+                            Console.Write('/');
+                            Console.Write(new string('^', n / 2));
+                            Console.WriteLine('\\');
+                        }
+                        else if (i == n - 1)
+                        {
+                            Console.Write('\\');
+                            Console.Write(new string('_', n / 2));
+                            Console.Write('/');
+                            Console.Write(new string(' ', n - 4));
+                            Console.Write('\\');
+                            Console.Write(new string('_', n / 2));
+                            Console.WriteLine('/');
+                        }
+                        else if (i == n - 2)
+                        {
+                            Console.Write("| ");
+                            Console.Write(new string(' ', n / 2));
+                            Console.Write(new string('_', n - 4));
+                            Console.Write(new string(' ', n / 2));
+                            Console.WriteLine(" |");
+                        }
+                        else
+                        {
+                            Console.Write("|");
+                            Console.Write(new string(' ', 2 * n - 2));
+                            Console.WriteLine("|");
+                        }
+                    }
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        Console.Write('/');
+                        Console.Write(new string('^', n / 2));
+                        Console.Write('\\');
+                        // Console.Write(new string('_', n / 2));
+                        Console.Write('/');
+                        Console.Write(new string('^', n / 2));
+                        Console.WriteLine('\\');
+                    }
+                    else if (i == n - 1)
+                    {
+                        Console.Write('\\');
+                        Console.Write(new string('_', n / 2));
+                        Console.Write('/');
+                        // Console.Write(new string(' ', n / 2));
+                        Console.Write('\\');
+                        Console.Write(new string('_', n / 2));
+                        Console.WriteLine('/');
+                    }
+                    else if (i == n - 2)
+                    {
+                        Console.Write("| ");
+                        Console.Write(new string(' ', n / 2));
+                        //Console.Write(new string('_', n / 2));
+                        Console.Write(new string(' ', n / 2));
+                        Console.WriteLine(" |");
+                    }
+                    else
+                    {
+                        Console.Write("|");
+                        Console.Write(new string(' ', 2 * n - 2));
+                        Console.WriteLine("|");
+                    }
+                }
+            }
+
+        }
+    }
+}
