35543a102deeb9025a983b6f8d43ad263f09a276
hristina.koleva <hristinakoleva@gmail.com>
2017-02-27 10:27:46 +0200

    6th Homework Christmas Tree done again

---
+++ SoftUniDrawingWithLoops/G_ChristmasTreeFromWork/ChristmasTreeFromWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace G_ChristmasTreeFromWork
+{
+    class ChristmasTreeFromWork
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n+1; i++)
+            {
+                Console.Write(new string(' ', n - i));
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                  
+                }
+               
+                Console.Write(" | ");
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
