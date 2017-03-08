53efb0f9160f456c25e16dfceefa16a96c367e88
hrisKoleva <hristinakoleva@gmail.com>
2017-03-08 21:42:33 +0200

    8TH Homework - Fibonacci Task - Done. Submitted.Passed

---
+++ SoftUniAdvancedLoops/L_Fibonacci/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace L_Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int firstNumber =1;
+            int secondNumber=1;
+            int nextNumber = 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                if(i==0 || i==1)
+                {
+                    firstNumber = 1;
+                    secondNumber = 1;
+                }
+                else
+                {
+                    nextNumber = firstNumber + secondNumber;
+                    firstNumber = secondNumber;
+                    secondNumber = nextNumber;
+                }
+            }
+            Console.WriteLine(nextNumber);
+        }
+    }
+}
