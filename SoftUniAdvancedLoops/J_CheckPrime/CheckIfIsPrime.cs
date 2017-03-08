70e83b05d65cac2c5e7005b2d901e65c20bdd9a3
hristina.koleva <hristinakoleva@gmail.com>
2017-03-08 12:44:43 +0200

    8Th Homework 10th Task IsPime- Done submitted  passed

---
+++ SoftUniAdvancedLoops/J_CheckPrime/CheckIfIsPrime.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace J_CheckPrime
+{
+    class CheckIfIsPrime
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+            int i = 2;
+
+            while (i <= n)
+            {
+                if(n%i!=0)
+                {
+                    i++;
+                }
+                else
+                {
+                    if (i == n)
+                    {
+                        isPrime = true;
+                    }
+                    else
+                    {
+                        isPrime = false;
+                       
+                    }
+                    break;
+                }
+            }
+
+            if (n<=0 || n==1)
+            {
+                Console.WriteLine("Not Prime");
+            }
+            else
+            {
+
+                if (isPrime)
+                {
+                    Console.WriteLine("Prime");
+
+                }
+                else
+                {
+                    Console.WriteLine("Not Prime");
+                }
+            }
+        }
+    }
+}
