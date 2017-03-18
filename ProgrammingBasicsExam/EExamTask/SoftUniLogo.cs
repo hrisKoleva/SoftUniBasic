8f41c47f5291cc9dc196151248e0550b0ba7a10c
hrisKoleva <hristinakoleva@gmail.com>
2017-03-18 17:52:32 +0200

    Exam done - I could only submit 5 of the task but now I solved the 6th too at home :-)

--- ProgrammingBasicsExam/EExamTask/SoftUniLogo.cs
+++ ProgrammingBasicsExam/EExamTask/SoftUniLogo.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EExamTask
 {
@@ -16,47 +12,46 @@ namespace EExamTask
             int columns = 12 * n - 5;
 
             int upperRows = rows / 2;
-            int lowerRows = rows - upperRows;
-
-            /*
-             * ...............#...............
-               ............#######............
-               .........#############.........
-               ......###################......
-               ...#########################...
-               ###############################*/
+            int lowerRows = rows - upperRows - n - 3;
 
             for (int i = 0; i <= upperRows; i++)
             {
                 Console.Write(new string('.', (12 * n - 5) / 2 - i * 3));
-                Console.Write(new string('#', ((12*n-5) - 2* ((12 * n - 5) / 2 - i * 3))));
+                Console.Write(new string('#', ((12 * n - 5) - 2 * ((12 * n - 5) / 2 - i * 3))));
                 Console.Write(new string('.', (12 * n - 5) / 2 - i * 3));
                 Console.WriteLine();
             }
 
-            /*
-               |..#########################...
+            int dots = 0;
+            int hashtags = 0;
 
-               |.....###################......
-               |.....###################......
-               @.....###################......	
-           */
-
-            for (int i = 0; i < n-2; i++)
+            for (int i = 0; i < n - 2; i++)
             {
-                Console.Write("|");
-                Console.Write(new string('.', (n - 1) + i * 3));
+                dots = 3 + 3 * i;
+                hashtags = columns - 2 * dots;
 
+                Console.Write("|");
+                Console.Write(new string('.', dots - 1));
+                Console.Write(new string('#', hashtags));
+                Console.Write(new string('.', dots));
+                Console.WriteLine();
             }
 
-
-            for (int i = 0; i < lowerRows; i++)
+            for (int i = 0; i <= n - 2; i++)
             {
                 Console.Write("|");
-
+                Console.Write(new string('.', (columns-(6*n+1))/2-1));
+                Console.Write(new string('#', 6*n+1));
+                Console.Write(new string('.', (columns - (6 * n + 1)) / 2));
+                Console.WriteLine();
             }
 
-
+            Console.Write("@");
+            Console.Write(new string('.', (columns - (6 * n + 1)) / 2 - 1));
+            Console.Write(new string('#', 6 * n + 1));
+            Console.Write(new string('.', (columns - (6 * n + 1)) / 2));
+            Console.WriteLine();
         }
     }
 }
+
