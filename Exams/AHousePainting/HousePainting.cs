0071831a034c07dc7675c791edd7724c26552f78
hristina.koleva <hristinakoleva@gmail.com>
2017-03-21 17:52:57 +0200

    HousePaint and Cups from March 19th morning done

---
+++ Exams/AHousePainting/HousePainting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AHousePainting
+{
+    class HousePainting
+    {
+        static void Main(string[] args)
+        {
+            double xHouseHight = double.Parse(Console.ReadLine());
+            double yHouseLength = double.Parse(Console.ReadLine());
+            double hRoofHeight = double.Parse(Console.ReadLine());
+
+            double greenAreaWallsWithoutDoorAndWindows = (xHouseHight*xHouseHight + xHouseHight*xHouseHight + xHouseHight*yHouseLength + xHouseHight*yHouseLength - 2.4 - 2*1.5*1.5);
+            double redAreaRoof = (xHouseHight * hRoofHeight + 2 * xHouseHight * yHouseLength);
+
+            Console.WriteLine($"{(greenAreaWallsWithoutDoorAndWindows/3.4).ToString("0.00")}");
+            Console.WriteLine($"{(redAreaRoof/4.3).ToString("0.00")}");
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Reflection;
+using System.Security.Permissions;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectDirectoryPath = @"C:\Nemo\Baseline\Bin\x86d\Kongsberg.Dory.ErsParameterMaps.dll";
+
+            Assembly domainAssembly = Assembly.LoadFrom(projectDirectoryPath);
+
+            var types = domainAssembly.GetTypes();
+
+
+            foreach (var item in types.OrderBy(x => x.Name))
+            {
+                if (!item.IsNested)
+                    Console.WriteLine(item.Name);
+
+            //    var methods = item.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            //    var fields = item.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            //    foreach (var method in methods.OrderBy(x => x.Name))
+            //    {
+
+            //        if (!method.Name.Equals("GetType") && !method.Name.StartsWith("<") &&
+            //            !method.Name.StartsWith("get_") && !method.Name.StartsWith("set_") &&
+            //            !method.Name.StartsWith("Member") && !method.Name.Equals("GetValue") &&
+            //            !method.Name.StartsWith("System") && !method.Name.Equals("ToString") &&
+            //            !method.Name.StartsWith("GetHashCode") && !method.Name.Equals("Finalize") &&
+            //            !method.Name.StartsWith("Equals") && !method.Name.Equals("HasFlag"))
+            //        {
+
+            //            Console.WriteLine("\t" + method.Name);
+                   
+                      
+            //        }
+
+            //    }
+            //    Console.WriteLine();
+
+            //    foreach (var field in fields.OrderBy(x => x.Name))
+            //    {
+            //        if (!field.Name.StartsWith("<"))
+            //        Console.WriteLine("\t\t" + field.Name);
+            //    }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
